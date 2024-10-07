63a9dc24b08928b38fa7d1f1d86671842ca6b7ae
Fabien <Fabienmaillard@proton.me>
2024-10-07 13:36:13 +0200

    Mise en place de l'architecture

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <p>&copy; <?php echo date("Y"); ?> Your Portfolio. All rights reserved.</p>
+</footer>
+</body>
+</html>
---
+++ main.php
@@ -0,0 +1,97 @@
+<div class="hero-section">
+    <div class="header">
+        <?php include 'header.php'; ?>
+    </div>
+    <img src="img/Logo FM.png" alt="Mon Logo">
+    <h1>Bienvenue</h1>
+    <button class="chevron">
+        <img src="img/Chevron_bas btn.png" alt="Chevron Bas">
+    </button>
+</div>
+<section class="container-fluid qui-je-suis">
+    <h2 class="text-center" id="qjs">Qui je suis</h2>
+        <div class="container">
+            <div class="row align-items-center">
+                <!-- Colonne de gauche avec l'image et le texte -->
+                <div class="col-md-4">
+                    <div class="image-placeholder bg-dark">
+                        <img src="img/moi.jpg" alt="Moi">
+                    </div>
+                    <p class="description">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        Hac habitasse platea dictumst quisque sagittis purus sit amet volutpat. Imperdiet proin fermentum leo vel orci porta non pulvinar neque. 
+                        Dolor sit amet consectetur adipiscing.
+                    </p>
+                </div>
+
+                <div class="col-md-8">
+                    <div class="row justify-content-center text-center">
+                    <div class="d-flex flex-column align-items-center">
+                        <div class="d-flex justify-content-around my-3">
+                            <div class="col-3">
+                                <div class="skill-box">
+                                    <img src="img/adobe.png" alt="Adobe">
+                                    <p class="skill-text">Suite Adobe</p>
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="skill-box">
+                                    <img src="img/adobe.png" alt="Adobe">
+                                    <p class="skill-text">HTML/CSS, JS, PHP</p>
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="skill-box">
+                                    <img src="img/adobe.png" alt="Adobe">
+                                    <p class="skill-text">WordPress</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="d-flex flex-nowrap">
+                            <div class="col">
+                                <div class="skill-box2">
+                                    <img src="img/adobe.png" alt="Adobe">
+                                    <p class="skill-text">Figma</p>
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="center-box">
+                                    <p class="center-text">Mes compétences <br> Mes passions</p>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="skill-box2">
+                                    <img src="img/adobe.png" alt="Adobe">
+                                    <p class="skill-text">Photographie</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="d-flex flex-column align-items-center">
+                        <div class="d-flex justify-content-around my-3">
+                            <div class="col-3">
+                                <div class="skill-box3">
+                                    <img src="img/adobe.png" alt="Adobe">
+                                    <p class="skill-text">Suite Adobe</p>
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="skill-box3">
+                                    <img src="img/adobe.png" alt="Adobe">
+                                    <p class="skill-text">HTML/CSS, JS, PHP</p>
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="skill-box3">
+                                    <img src="img/adobe.png" alt="Adobe">
+                                    <p class="skill-text">WordPress</p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
