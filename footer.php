c36d267f2c447b781d2890e59a245525a4ec5972
Fabien <Fabienmaillard@proton.me>
2024-10-08 08:47:53 +0200

    Refactor LESS file and update CSS output path

--- footer.php
+++ footer.php
@@ -1,5 +1,31 @@
 <footer>
-    <p>&copy; <?php echo date("Y"); ?> Your Portfolio. All rights reserved.</p>
+  <div class="footer-container">
+    <div class="footer-column">
+      <ul>
+        <li><a href="#">Accueil</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </div>
+    <div class="footer-column">
+      <ul>
+        <li><a href="#">Mentions légales</a></li>
+        <li><a href="#">Politique de confidentialité</a></li>
+      </ul>
+    </div>
+    <div class="footer-column">
+      <ul>
+        <li><a href="#">Condition d'utilisation</a></li>
+        <li><a href="#">Cookies</a></li>
+      </ul>
+    </div>
+    <div class="footer-social row">
+      <a href="#"><img src="img/linkedin.png" alt="LinkedIn"></a>
+      <a href="#"><img src="img/instagram.png" alt="Instagram"></a>
+    </div>
+  </div>
+<div class="footer-copyright">
+    ©<?php echo date("Y"); ?> - MAILLARD Fabien
+</div>
 </footer>
 </body>
 </html>
--- main.php
+++ main.php
@@ -1,159 +1,162 @@
-<div class="hero-section">
-    <div class="header">
-        <?php include 'header.php'; ?>
+    <div class="hero-section">
+        <div class="header">
+            <?php include 'header.php'; ?>
+        </div>
+        <img src="img/Logo FM.png" alt="Mon Logo">
+        <h1>Bienvenue</h1>
+        <button class="chevron">
+            <img src="img/Chevron_bas btn.png" alt="Chevron Bas">
+        </button>
     </div>
-    <img src="img/Logo FM.png" alt="Mon Logo">
-    <h1>Bienvenue</h1>
-    <button class="chevron">
-        <img src="img/Chevron_bas btn.png" alt="Chevron Bas">
-    </button>
-</div>
 
 
-<!-- Section Qui je suis avec PHOTO + BENTO -->
-<section class="container-fluid qui-je-suis">
-    <h2 class="text-center ss-titre" id="ss-titre">Qui je suis</h2>
-        <div class="container">
-            <div class="row align-items-center">
-                <!-- Colonne de gauche avec l'image et le texte -->
-                <div class="col-md-4">
-                    <div class="image-placeholder bg-dark">
-                        <img src="img/moi.jpg" alt="Moi">
+    <!-- Section Qui je suis avec PHOTO + BENTO -->
+    <section class="container-fluid qui-je-suis">
+        <h2 class="text-center" id="ss-titre">Qui je suis</h2>
+            <div class="container">
+                <div class="row align-items-center">
+                    <!-- Colonne de gauche avec l'image et le texte -->
+                    <div class="col-md-4">
+                        <div class="image-placeholder bg-dark">
+                            <img src="img/moi.jpg" alt="Moi">
+                        </div>
+                        <p class="description">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                            Hac habitasse platea dictumst quisque sagittis purus sit amet volutpat. Imperdiet proin fermentum leo vel orci porta non pulvinar neque. 
+                            Dolor sit amet consectetur adipiscing.
+                        </p>
                     </div>
-                    <p class="description">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                        Hac habitasse platea dictumst quisque sagittis purus sit amet volutpat. Imperdiet proin fermentum leo vel orci porta non pulvinar neque. 
-                        Dolor sit amet consectetur adipiscing.
-                    </p>
-                </div>
-                <!-- Fin colonne Photo
-                 Début BENTO -->
-                <div class="col-md-8">
-                    <div class="row g-2">
-                    <div class="row g-2">
-                            <div class="col">
-                                <div class="skill-box">
-                                    <img src="img/adobe.png" alt="Adobe">
-                                    <p class="skill-text">Suite Adobe</p>
-                                </div>
-                            </div>
-                            <div class="col">
-                                <div class="skill-box">
-                                    <img src="img/code.png" alt="code">
-                                    <p class="skill-text">HTML/CSS, JS, PHP</p>
-                                </div>
-                            </div>
-                            <div class="col">
-                                <div class="skill-box">
-                                    <img src="img/wordpress.webp" alt="Wordpress">
-                                    <p class="skill-text">WordPress</p>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- FIN Première ligne bento -->
-
+                    <!-- Fin colonne Photo
+                    Début BENTO -->
+                    <div class="col-md-8">
                         <div class="row g-2">
-                            <div class="col-3">
-                                <div class="skill-box2">
-                                    <img src="img/figma.webp" alt="Figma">
-                                    <p class="skill-text">Figma</p>
-                                </div>
-                            </div>
-                            <div class="col">
-                                <div class="center-box">
-                                    <p class="skill-text">Mes competences <br> Mes passions</p>
-                                </div>
-                            </div>
-                            <div class="col-3">
-                                <div class="skill-box2">
-                                    <img src="img/adobe.png" alt="Adobe">
-                                    <p class="skill-text">BOX L2 C3</p>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- FIN Deuxième ligne bento -->
-
                         <div class="row g-2">
-                            <div class="col">
-                                <div class="skill-box3">
-                                    <img src="img/adobe.png" alt="Adobe">
-                                    <p class="skill-text">BOX L3 C1</p>
+                                <div class="col">
+                                    <div class="skill-box">
+                                        <img src="img/adobe.png" alt="Adobe">
+                                        <p class="skill-text">Suite Adobe</p>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="skill-box">
+                                        <img src="img/code.png" alt="code">
+                                        <p class="skill-text">HTML/CSS, JS, PHP</p>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="skill-box">
+                                        <img src="img/wordpress.webp" alt="Wordpress">
+                                        <p class="skill-text">WordPress</p>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="col">
-                                <div class="skill-box3">
-                                    <img src="img/adobe.png" alt="Adobe">
-                                    <p class="skill-text">BOX L3 C2</p>
-                                </div>
-                            </div>
-                            <div class="col">
-                                <div class="skill-box3">
-                                    <img src="img/adobe.png" alt="Adobe">
-                                    <p class="skill-text">BOX L3 C3</p>
-                                </div>
-                            </div>
-                            <div class="col">
-                                <div class="skill-box3">
-                                    <img src="img/adobe.png" alt="Adobe">
-                                    <p class="skill-text">BOX L3 C4</p>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- FIN Troisième ligne bento -->
+                            <!-- FIN Première ligne bento -->
 
+                            <div class="row g-2">
+                                <div class="col-3">
+                                    <div class="skill-box2">
+                                        <img src="img/figma.webp" alt="Figma">
+                                        <p class="skill-text">Figma</p>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="center-box">
+                                        <p class="skill-text2">Mes competences <br> Mes passions</p>
+                                    </div>
+                                </div>
+                                <div class="col-3">
+                                    <div class="skill-box2">
+                                        <img src="img/adobe.png" alt="Adobe">
+                                        <p class="skill-text">BOX L2 C3</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- FIN Deuxième ligne bento -->
+
+                            <div class="row g-2">
+                                <div class="col">
+                                    <div class="skill-box3">
+                                        <img src="img/adobe.png" alt="Adobe">
+                                        <p class="skill-text">BOX L3 C1</p>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="skill-box3">
+                                        <img src="img/adobe.png" alt="Adobe">
+                                        <p class="skill-text">BOX L3 C2</p>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="skill-box3">
+                                        <img src="img/adobe.png" alt="Adobe">
+                                        <p class="skill-text">BOX L3 C3</p>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="skill-box3">
+                                        <img src="img/adobe.png" alt="Adobe">
+                                        <p class="skill-text">BOX L3 C4</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- FIN Troisième ligne bento -->
+
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
-    <!-- FIN SECTION PHOTO BENTO
-     
-    Début "Mes travaux"-->
-    <section>
-        <div>
-            <h2 class="text-center" id="ss-titre">Découvrir mes travaux</h2>
+        </section>
+        <!-- FIN SECTION PHOTO BENTO
+        
+        Début "Mes travaux"-->
+        <section>
+            <div>
+                <h2 class="text-center" id="ss-titre">Découvrir mes travaux</h2>
+            </div>
+
+            <div class="container text-center mt-5">
+        <div class="row">
+        <!-- Logo et Identité Visuelle -->
+        <div class="col-md-6">
+            <div class="portfolio-item">
+                <h3>Logos et Identité visuelle</h3>
+                <img src="img/id_logo.png" alt="Logos et Identité visuelle">
+            </div>
         </div>
 
-        <div class="container text-center mt-5">
-    <div class="row">
-      <!-- Logo et Identité Visuelle -->
-      <div class="col-md-6">
-        <div class="portfolio-item">
-          <img src="image1.jpg" alt="Logos et Identité visuelle" class="img-fluid rounded-circle">
-          <h3>Logos et Identité visuelle</h3>
+        <!-- Sites web et Maquettes -->
+        <div class="col-md-6">
+            <div class="portfolio-item">
+            <h3>Sites web et Maquettes</h3>
+            <img src="img/site.png" alt="Sites web et Maquettes">
+            </div>
         </div>
-      </div>
+        </div>
 
-      <!-- Sites web et Maquettes -->
-      <div class="col-md-6">
-        <div class="portfolio-item">
-          <img src="image2.jpg" alt="Sites web et Maquettes" class="img-fluid rounded-circle">
-          <h3>Sites web et Maquettes</h3>
+        <div class="row mt-4">
+        <!-- Photographies -->
+        <div class="col-md-6">
+            <div class="portfolio-item">
+            <h3>Photographies</h3>
+            <img src="img/photo.png" alt="Photographies">
+            </div>
         </div>
-      </div>
+
+        <!-- Vidéos -->
+        <div class="col-md-6">
+            <div class="portfolio-item">
+                <h3>Vidéos</h3>
+                <img src="img/vidéo.png" alt="Vidéos">
+            </div>
+        </div>
+        </div>
     </div>
 
-    <div class="row mt-4">
-      <!-- Photographies -->
-      <div class="col-md-6">
-        <div class="portfolio-item">
-          <img src="image3.jpg" alt="Photographies" class="img-fluid rounded-circle">
-          <h3>Photographies</h3>
-        </div>
-      </div>
-
-      <!-- Vidéos -->
-      <div class="col-md-6">
-        <div class="portfolio-item">
-          <img src="image4.jpg" alt="Vidéos" class="img-fluid rounded-circle">
-          <h3>Vidéos</h3>
-        </div>
-      </div>
-    </div>
-  </div>
-
     </section>
 
 
 
-    </body>
+    <!-- Footer -->
+    <div class="footer">
+        <?php include 'footer.php'; ?>
+    </div>
