ce7a6d211ce4e8d25f3f45a08184632601a4317c
Jexien <fabienmaillard@proton.me>
2024-10-25 13:25:23 +0200

    Mobile version de l'index et tentative changement couleurs contact

--- contactbis.php
+++ contactbis.php
@@ -1,7 +1,7 @@
 <?php include 'header.php'; ?>
 
 <div class="d-flex justify-content-center">
-    <h1 class="col-6 my-5 text-center">Formulaire de contact</h1>
+    <h1 class="col-6 my-5 text-center bish1">Formulaire de contact</h1>
 </div>
 <div class="d-flex justify-content-center">
     <p class="notice col-6 text-center bistxt">Merci pour votre message. Nous avons bien pris en compte votre demande, vous recevrez une confirmation par mail dans les minutes qui viennent.</p>
