50d45d602e5cbc8a60acf18335c3d27ebe77da2a
Jexien <fabienmaillard@proton.me>
2024-11-20 19:08:55 +0100

    page webmaquette et website

---
+++ maquettes.php
@@ -0,0 +1,25 @@
+<?php
+include 'header.php';
+?>
+
+<div class="iframe-container">
+  <div class="item">
+    <iframe class="iframe-figma" src="https://embed.figma.com/proto/0RzUgQ1fkgDgVjvZHfz12P/3D?page-id=0%3A1&node-id=97-323&embed-host=share" allowfullscreen></iframe>
+    <p class="maquette">Maquette de proposition pour un nouveau site web pour l'entreprise ACS</p>
+  </div>
+
+  <div class="item">
+  <iframe class="iframe-figma" src="https://embed.figma.com/design/xiebEHTqhKmucrjW3ubrTg/CMS---MAQUETTE?node-id=1-2&embed-host=share" allowfullscreen></iframe>
+    <p class="maquette">Wireframe réalisée en quelques heures servant de base pour un site web construit en CMS (Wordpress), à l'aide d'un sous-thème perso, d'HTML, de CSS et de PHP</p>
+    <p class="maquette"><a href="https://pulse.fabien-maillard.mds-grenoble.yt/index.php/index/" target="_blank">Ce site est visible à partir du lien suivant ce lien</a>.</p>
+  </div>
+
+  <div class="item">
+  <iframe class="iframe-figma" src="https://embed.figma.com/design/bGiEzGBmzqFnYcDNPl1EAq/Boulangerie-2?node-id=362-724&embed-host=share" allowfullscreen></iframe>
+    <p class="maquette">Maquette du site internet de la boulangerie "Le pain de Mayou" située à Saint jean de chevelu. Actuellement le site web est en cours de développement par une agence</p>
+  </div>
+</div>
+
+<?php
+include 'footer.php';
+?>
--- website.php
+++ website.php
@@ -4,7 +4,7 @@ include 'header.php';
 
 
 <p class="websitetitle">ATTENTION : Visualisation déconseillée sur téléphone. En effet, le site suivant ne dispose pas de responsive</p>
-<iframe src="img/Elseworld/index.html" width="960px" height="540px" style="border:none;"></iframe>
+<iframe class="iframeweb" src="img/Elseworld/index.html" width="960px" height="540px" style="border:none;"></iframe>
 <p class="websitetxt"> Réalisé dans le cadre de mes études, ce projet visait seulement à reproduire le design d'une maquette. En conséquence, elle n'intègre pas de fonctionnalités interactives, comme le menu déroulant ou autres éléments</p>
 <a href="webmaquette.php" class="btn btn-primary">Retour</a>
 <?php
